e3819ffc5944774e7deac6bdbbfe13948d0e78e7
too-much-magic <dylancrombie@outlook.com>
2025-03-15 23:23:04 -0600

    User may now add tasks.

--- task-cli.py
+++ task-cli.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import json
+import datetime
 args = sys.argv
 
 
@@ -12,9 +13,24 @@ if not os.path.exists("tasks.json"):
     _ = open("tasks.json", mode="w")
 
 with open("tasks.json") as file:
-    tasks: dict = json.loads(file.read())
+    try:
+        tasks = json.loads(file.read())
+    except json.decoder.JSONDecodeError:
+        tasks = []
 
 match args[1]:
     case "add":
-        if len(args) != 2:
+        if len(args) != 3:
             print("Please enter which task you would like to add.")
+            quit(1)
+        curr = {
+                "id": len(tasks) + 1,
+                "description": args[2],
+                "status": "todo",
+                "createdAt": datetime.datetime.now().strftime("%c"),
+                "updatedAt": datetime.datetime.now().strftime("%c")
+        }
+        tasks.append(curr)
+        with open("tasks.json", 'w') as file:
+            file.write(json.dumps(tasks))
+        print(f"{args[2]} added successfully!")
